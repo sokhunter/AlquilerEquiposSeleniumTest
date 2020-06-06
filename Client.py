a1c1eda56f6fc960289bd69f9b2b05d248f2a45e
Stefany Livaque <emylivaque@gmail.com>
2020-06-05 19:51:35 -0500

    Pruebas del modulo de cliente

--- Client.py
+++ Client.py
@@ -1,64 +1,52 @@
 import unittest
-import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from helpers.auth_helper import *
+from helpers.client_helper import *
 
 class Client(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Edge(executable_path=r"C:\Users\emyli\Desktop\Stefany\Cursos\Python\selenium\edgedriver_win64/msedgedriver.exe")
 		self.driver.get("https://webapplicationalquilerequipos20200602204639.azurewebsites.net/Account/login?ReturnUrl=%2f")
 		self.driver.implicitly_wait(5)
-		driver.find_element_by_id("Login_txt_NameUser").send_keys("knox")
-		driver.find_element_by_id("Login_txt_Password").send_keys("admin")
-		driver.find_element_by_id("Login_btn_Login").click()
-		self.driver.implicitly_wait(5)
+		authHelper = Auth_helper(self.driver)
+		authHelper.login('knox', 'admin')
 
 	def test_list_clients(self):
 		driver = self.driver
-		driver.find_element_by_id("Layout_btn_Cliente").click()
-		driver.implicitly_wait(5)
-		title = driver.find_element_by_xpath('//*[@id="content"]/div/h4/strong').text
-		self.assertEqual(title, "Clientes")
+		clientHelper = Client_helper(driver)
+		clientHelper.list()
+		clientHelper.validate_list_title()
 
 	def test_add_client(self):
 		driver = self.driver
-		driver.find_element_by_id("Layout_btn_Cliente").click()
-		driver.implicitly_wait(5)
+		clientHelper = Client_helper(driver)
+		clientHelper.list()
 		driver.find_element_by_id("Cliente_btn_Registrar").click()
 		driver.implicitly_wait(5)
-		driver.find_element_by_id("Cliente_txt_Nombre").send_keys("Emily Rojas")
-		driver.find_element_by_id("Cliente_txt_Documento").send_keys("73829302")
-		driver.find_element_by_id("Cliente_btn_Guardar").click()
-		driver.implicitly_wait(5)
-		title = driver.find_element_by_xpath('//*[@id="content"]/div/h4/strong').text
-		self.assertEqual(title, "Clientes")
+		clientHelper.save('danny phantom', '12345678')
+		clientHelper.validate_list_title()
 
 	def test_edit_client(self):
 		driver = self.driver
-		driver.find_element_by_id("Layout_btn_Cliente").click()
-		driver.implicitly_wait(5)
+		clientHelper = Client_helper(driver)
+		clientHelper.list()
 		driver.find_element_by_id("Cliente_btn_Editar").click()
 		driver.implicitly_wait(5)
-		driver.find_element_by_id("Cliente_txt_Nombre").send_keys("Emily Rojas")
-		driver.find_element_by_id("Cliente_txt_Documento").send_keys("73829302")
-		driver.find_element_by_id("Cliente_btn_Guardar").click()
-		driver.implicitly_wait(5)
-		title = driver.find_element_by_xpath('//*[@id="content"]/div/h4/strong').text
-		self.assertEqual(title, "Clientes")
+		clientHelper.save('Leonardo Ramos Ticona', '72331234')
+		clientHelper.validate_list_title()
+		
 
 	def test_delete_client(self):
 		driver = self.driver
-		driver.find_element_by_id("Layout_btn_Cliente").click()
-		driver.implicitly_wait(5)
+		clientHelper = Client_helper(driver)
+		clientHelper.list()
 		driver.find_element_by_id("Cliente_btn_Eliminar").click()
 		driver.implicitly_wait(5)
 		driver.find_element_by_id("Cliente_btn_Eliminar").click()
 		driver.implicitly_wait(5)
-		title = driver.find_element_by_xpath('//*[@id="content"]/div/h4/strong').text
-		self.assertEqual(title, "Clientes")
+		clientHelper.validate_list_title()
 
 	def tearDown(self):
-		# time.sleep(5);
 		self.driver.close()
 
 if __name__ == '__main__':
---
+++ helpers/client_helper.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium.webdriver.support.ui import Select
+
+class Client_helper():
+	def __init__(self, driver):
+		self.driver = driver
+
+	def validate_list_title(self):
+		my_assertion = unittest.TestCase('__init__')
+		title = self.driver.find_element_by_xpath('//*[@id="content"]/div/h4/strong').text
+		my_assertion.assertEqual(title, "Clientes")
+	
+	def list(self):
+		my_assertion = unittest.TestCase('__init__')
+		driver = self.driver
+		driver.find_element_by_id("Layout_btn_Cliente").click()
+		driver.implicitly_wait(5)
+
+	def save(self, name, document):
+		my_assertion = unittest.TestCase('__init__')
+		driver = self.driver
+
+		txtName = driver.find_element_by_id("Cliente_txt_Nombre")
+		txtName.clear()
+		txtName.send_keys(name)
+
+		cbxDocument = Select(driver.find_element_by_id("Cliente_ddl_TipoDocumento"))
+		cbxDocument.select_by_index(2)
+
+		txtDocumento = driver.find_element_by_id("Cliente_txt_Documento")
+		txtDocumento.clear()
+		txtDocumento.send_keys(document)
+
+		driver.find_element_by_id("Cliente_btn_Guardar").click()
+		driver.implicitly_wait(5)
+
+		
